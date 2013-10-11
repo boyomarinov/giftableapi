9917284e0cc9dbb8caa003fe9ef03c120e275286
boyomarinov <boyan.marinov@yahoo.com>
2013-10-11 05:28:24 -0700

    init

---
+++ GiftableApi/Giftable.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Giftable.Models;
+
+namespace Giftable.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("GiftableDb")
+        {
+        }
+
+        public IDbSet<User> Users { get; set; }
+        public IDbSet<Circle> Circles { get; set; }
+        public IDbSet<Gift> Gifts { get; set; }
+    }
+}
---
+++ GiftableApi/Giftable.Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giftable.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string AuthenticationCode { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public virtual ICollection<Gift> Wishlist { get; set; } 
+
+        public virtual ICollection<Gift> SuggestedGifts { get; set; } 
+    }
+}
