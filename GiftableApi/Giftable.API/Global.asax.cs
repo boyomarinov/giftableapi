85b9b99c7899230e4826dc15e9651b43f0e4e0e5
boyomarinov <boyan.marinov@yahoo.com>
2013-10-12 08:08:09 -0700

    added database initializer

--- GiftableApi/Giftable.API/Global.asax.cs
+++ GiftableApi/Giftable.API/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Giftable.Data;
+using Giftable.Data.Migrations;
 
 namespace Giftable.API
 {
@@ -22,6 +25,8 @@ namespace Giftable.API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
     }
 }
